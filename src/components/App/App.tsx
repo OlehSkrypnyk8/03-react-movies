cf1eb67084b489c3bb06c23d25b4edf36ae4f6f4
oleh skrypnyk <oleh.skrypnyk@ukr.net>
2025-12-13 18:43:00 +0200

    Created movieGrid, modalMovie, updated App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,48 +1,59 @@
-import { useEffect, useState } from "react";
 import fetchMovies from "../../services/movieService";
 import type { Movie } from "../../types/movie";
 import styles from "./App.module.css";
 import SearchBar from "../SearchBar/SearchBar";
+import MovieModal from "../MovieModal/MovieModal";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<string | null>(null);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-  useEffect(() => {
-    setLoading(true);
-    fetchMovies("Inception")
-      .then((data) => setMovies(data))
-      .catch((err) => setError(err.message))
-      .finally(() => setLoading(false));
-  }, []);
+  const handleSearch = async (query: string) => {
+    setIsLoading(true);
+    setError(false);
 
-  const handleSearchSubmit = (query: string) => {
-    setLoading(true);
-    fetchMovies(query)
-      .then((data) => setMovies(data))
-      .catch((err) => setError(err.message))
-      .finally(() => setLoading(false));
+    try {
+      const results = await fetchMovies(query);
+      setMovies(results);
+
+      if (results.length === 0) {
+        toast.error("No movies found for your request.");
+      }
+    } catch {
+      setError(true);
+    } finally {
+      setIsLoading(false);
+    }
   };
+
+  const handleSelectMovie = (movie: Movie) => {
+    setSelectedMovie(movie);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedMovie(null);
+  };
+
   return (
     <div className={styles.app}>
-      <SearchBar onSubmit={handleSearchSubmit} />
-      {loading && <p>Loading...</p>}
-      {error && <p className={styles.error}></p>}
-      <ul className={styles.movieList}>
-        {movies.map((movie) => (
-          <li key={movie.id} className={styles.movieItem}>
-            <h2>{movie.title}</h2>
-            {movie.release_date && <p>Release: {movie.release_date}</p>}
-            {movie.poster_path && (
-              <img
-                src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-                alt={movie.title}
-              />
-            )}
-          </li>
-        ))}
-      </ul>
+      <SearchBar onSubmit={handleSearch} />
+      <Toaster position="top-center" />
+
+      {isLoading && <Loader />}
+      {error && <ErrorMessage />}
+      {movies.length > 0 && (
+        <MovieGrid movies={movies} onSelect={handleSelectMovie} />
+      )}
+      {selectedMovie && (
+        <MovieModal movie={selectedMovie} onClose={handleCloseModal} />
+      )}
     </div>
   );
 }
--- src/components/MovieModal/MovieModal.tsx
+++ src/components/MovieModal/MovieModal.tsx
@@ -4,11 +4,11 @@ import type { Movie } from "../../types/movie";
 import { createPortal } from "react-dom";
 
 interface MovieModalProps {
-  movie: Movie | null;
+  movie: Movie;
   onClose: () => void;
 }
 
-function MovieModal({ onClose }: MovieModalProps) {
+function MovieModal({ movie, onClose }: MovieModalProps) {
   useEffect(() => {
     function handleKeyDown(event: KeyboardEvent) {
       if (event.key === "Escape") {
@@ -20,8 +20,18 @@ function MovieModal({ onClose }: MovieModalProps) {
       document.removeEventListener("keydown", handleKeyDown);
     };
   }, [onClose]);
+  const handleBackdropClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
   return createPortal(
-    <div className={css.backdrop} role="dialog" aria-modal="true">
+    <div
+      className={css.backdrop}
+      role="dialog"
+      aria-modal="true"
+      onClick={handleBackdropClick}
+    >
       <div className={css.modal}>
         <button
           className={css.closeButton}
@@ -30,19 +40,21 @@ function MovieModal({ onClose }: MovieModalProps) {
         >
           &times;
         </button>
-        <img
-          src="https://image.tmdb.org/t/p/original/backdrop_path"
-          alt="movie_title"
-          className={css.image}
-        />
+        {movie.backdrop_path && (
+          <img
+            src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+            alt={movie.title}
+            className={css.image}
+          />
+        )}
         <div className={css.content}>
-          <h2>movie_title</h2>
-          <p>movie_overview</p>
+          <h2>{movie.title}</h2>
+          <p>{movie.overview}</p>
           <p>
-            <strong>Release Date:</strong> movie_release_date
+            <strong>Release Date:</strong> {movie.release_date}
           </p>
           <p>
-            <strong>Rating:</strong> movie_vote_average/10
+            <strong>Rating:</strong> {movie.vote_average}/10
           </p>
         </div>
       </div>
@@ -50,4 +62,5 @@ function MovieModal({ onClose }: MovieModalProps) {
     document.body
   );
 }
+
 export default MovieModal;
